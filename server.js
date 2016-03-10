414bd55db4347b24b7bea1e9b85b7fccdce5aad6
Rory Sterley <rorysterley@gmail.com>
2016-03-10 08:11:16 -0800

    init

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// REQUIRE ====================================================================
+var express = require('express');
+
+// SETTUP =====================================================================
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + app));
+
+// SERVER INIT================================================================
+app.listen(port, function() {
+  console.log('\n==================================\n' +
+              '= Server listening on port: ' + port + ' =' +
+              '\n==================================\n');
+});

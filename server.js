c3e263dbd91f4ad746320605bc54ef63751eec20
Rory Sterley <rorysterley@gmail.com>
2016-03-10 08:32:07 -0800

    Fix phasers unholy coding conventions... and bug fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + app));
+app.use(express.static(__dirname + '/app'));
 
 // SERVER INIT================================================================
 app.listen(port, function() {
